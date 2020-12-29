2fb78a35d6ff11979cfa99e831487b51afefcc4f
technisculpt <info@technisculpt.com>
2020-12-30 09:38:43 +1100

    printing out pixels and comparing

--- main.py
+++ main.py
@@ -7,6 +7,24 @@ import os
 
 os.chdir('test')
 print(os.getcwd())
+
+x = np.uint8(9)
+
+
+skipFirst = True
 for filename in os.listdir(os.getcwd()):
    with open(os.path.join(os.getcwd(), filename), 'r') as f: # open in readonly mode
-      print(filename)
+      img = cv2.imread(filename)
+
+      rows,cols,layers = img.shape
+
+      if(skipFirst):
+         skipFirst = False
+      else:
+         for i in range(rows):
+            for j in range(cols):
+               for k in range(layers):
+                  test = img[i,j,k]
+                  if(test> x ):
+                     print(test)
+                     
