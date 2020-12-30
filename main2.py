496784863025d69d5619dc1d8f9649a782d6dcf9
technisculpt <info@technisculpt.com>
2020-12-31 01:59:30 +1100

    ready to test bit slow on lap-1

--- main2.py
+++ main2.py
@@ -2,27 +2,30 @@ import cv2
 import numpy as np
 import os
 
-
-os.chdir('test')
 img1 = cv2.imread('background.JPG')
-img2 = cv2.imread('IMG_8676.JPG')
+
+rows,cols,layers = img1.shape
+outgoing_image = np.zeros((rows,cols,3), np.uint8)
 
 x = np.uint8(9)
+os.chdir('test') 
+thresh = 2
 
-skipFirst = True
 for filename in os.listdir(os.getcwd()):
    with open(os.path.join(os.getcwd(), filename), 'r') as f: # open in readonly mode
       img = cv2.imread(filename)
+      for i in range(rows):
+         for j in range(cols):
+            bg_ch_1 = img1[i,j][0]
+            bg_ch_2 = img1[i,j][1]
+            bg_ch_3 = img1[i,j][2]
+            fg_ch_1 = img[i,j][0]
+            fg_ch_2 = img[i,j][1]
+            fg_ch_3 = img[i,j][2]
 
-      rows,cols,layers = img.shape
+            if(((fg_ch_1 - bg_ch_1) < thresh) and ((fg_ch_2 - bg_ch_2) < thresh) and ((fg_ch_3 - bg_ch_3) < thresh) ):
+               outgoing_image[i,j] = 255
 
-      if(skipFirst):
-         skipFirst = False
-      else:
-         for i in range(rows):
-            for j in range(cols):
-               for k in range(layers):
-                  test = img[i,j,k]
-                  if(test> x ):
-                     print(test)
-                     
+os.chdir('../output') 
+filename = 'out.jpg'
+cv2.imwrite(filename, outgoing_image)
